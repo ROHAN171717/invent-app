7f501e8925abb850861cb623d375538bb29815ae
RONS LAKHANI <you@example.com>
2022-12-16 01:05:49 +0530

    some changes

--- backend/utils/cloudinary.js
+++
@@ -1,9 +0,0 @@
-const cloudinary = require('cloudinary');
-
-cloudinary.config({
-  cloud_name: 'dyoq8stgr',
-  api_key: '196779951973952',
-  api_secret: '2NpoAIaBwD0HHfX-hEr-ERGDIDI'
-});
-
-module.exports = cloudinary;
